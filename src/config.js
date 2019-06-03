91f70443bc2bf1bf75bf25345ece927855bf8aad
John D. Aynedjian <johndavid5@yahoo.com>
2019-06-02 22:20:37 -0400

    set config.js verbosity to WARN...

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 export const config = {
-    //DEBUG_LEVEL: 'error',
-    //DEBUG_LEVEL: 'warn',
-    //DEBUG_LEVEL: 'info',
-    DEBUG_LEVEL: 'debug',
+    // DEBUG_LEVEL: 'error',
+    DEBUG_LEVEL: 'warn',
+    // DEBUG_LEVEL: 'info',
+    // DEBUG_LEVEL: 'debug',
     //DEBUG_LEVEL: 'trace', // trace not recommended...crashes logajohn=>logatim...unless we fix logatim...
 
     PORT: 3005,
 
-    // POSTS_URL: Used by ./src/server/model/posts...
+    // POSTS_URL: Used by ./src/server/model/posts as default source of posts...
     POSTS_URL: 'http://jsonplaceholder.typicode.com/posts'
 }
